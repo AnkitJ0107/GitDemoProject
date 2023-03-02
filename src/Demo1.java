5828e619a7a85da0028c859f8a8349d43f6bbeec
Ankit Jha <ankit.jha@neosoftmail.com>
2023-03-02 16:27:06 +0530

    updated

---
+++ src/Demo1.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo1 {
+    public static void main(String[] args) {
+        List<String> strings = Arrays.asList(args);
+        System.out.println(strings);
+
+    }
+}
